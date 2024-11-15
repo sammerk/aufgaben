12344d4735d092ae18c9439b6150df66fdc94468
Samuel Merk <samuel.merk@uni-tuebingen.de>
2024-11-15 22:55:58 +0100

    adsf

--- Man_Testing_Script.R
+++ Man_Testing_Script.R
@@ -1,4 +1,4 @@
 library(exams2forms)
 
-exams2webquiz("/Users/samuelmerk/Nextcloud/Lehre/aufgaben/gruppenunterschiede/guessing-u1-sc/exams/guessing-u1-sc.Rmd", 
+exams2webquiz("/Users/samuelmerk/Nextcloud/Lehre/aufgaben/gruppenunterschiede/guessing-cliff-d/exams/guessing-cliff-d.Rmd", 
               n = 25)
